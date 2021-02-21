d2df4aa2e80803942b89b75141c55ea6d418541c
ind-spirit <error156@yandex.ru>
2021-02-21 20:18:31 +0700

    completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array !== undefined) {
+        if (array.length > 0) {
+            let min = array[0];
+            for (let i = 0; i < array.length; i++) {
+                if (array[i] < min) {
+                    min = array[i];
+                }
+            }
+            return min;
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array !== undefined) {
+        if (array.length > 0) {
+            let max = array[0];
+            for (let i = 0; i < array.length; i++) {
+                if (array[i] > max) {
+                    max = array[i];
+                }
+            }
+            return max;
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array !== undefined) {
+        if (array.length > 0) {
+            let avg = 0,
+                i;
+
+            for (i = 0; i < array.length; i++) {
+                avg += array[i];
+            }
+            avg /= i;
+
+            return avg;
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
 }
